b0fee939570cc9a04208f58f56d0c2d1601c1ce4
Ang Xu <angxu1234@gmail.com>
2019-07-28 12:50:01 -0500

    heroku put boolean mappers fix

--- api/mappers.js
+++ api/mappers.js
@@ -2,7 +2,8 @@ module.exports = {
 	displayTrueFalse: function(object) {
 		return {
 			...object,
-			attending: object.attending === 1 ? true : false
+			attending:
+				object.attending === 1 || object.attending === true ? true : false
 		};
 	}
 };
