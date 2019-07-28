2147ea28d60474504a454db8ff434ad0179aaee9
Ang Xu <angxu1234@gmail.com>
2019-07-28 07:40:18 -0500

    deploy setup

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,7 @@
 const knex = require("knex");
 
-const config = require("../knexfile.js");
+const config = require("../knexfile");
 
-module.exports = knex(config.development);
+const dbEnv = process.env.DB_ENV || "development";
+
+module.exports = knex(config[dbEnv]);
