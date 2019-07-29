f8d3015616eac584d0521cc5f64d30af43799d26
Ang Xu <angxu1234@gmail.com>
2019-07-29 18:34:40 -0500

    seed syntax fix

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,22 +1,19 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-	// Deletes ALL existing entries
-	return knex("users").then(function() {
-		// Inserts seed entries
-		return knex("users").insert([
-			{
-				username: "example1",
-				password: bcrypt.hashSync("pass", 10),
-				full_name: "Chad",
-				email: "chad@hotmail.com"
-			},
-			{
-				username: "example2",
-				password: bcrypt.hashSync("pass", 10),
-				full_name: "Carol",
-				email: "carol@yahoo.com"
-			}
-		]);
-	});
+	// Inserts seed entries
+	return knex("users").insert([
+		{
+			username: "example1",
+			password: bcrypt.hashSync("pass", 10),
+			full_name: "Chad",
+			email: "chad@hotmail.com"
+		},
+		{
+			username: "example2",
+			password: bcrypt.hashSync("pass", 10),
+			full_name: "Carol",
+			email: "carol@yahoo.com"
+		}
+	]);
 };
--- data/seeds/004-recipes.js
+++ data/seeds/004-recipes.js
@@ -1,23 +1,20 @@
 exports.seed = function(knex) {
-	// Deletes ALL existing entries
-	return knex("recipes").then(function() {
-		// Inserts seed entries
-		return knex("recipes").insert([
-			{
-				event_id: 1,
-				recipe_name: "Pizza",
-				user_id: null
-			},
-			{
-				event_id: 1,
-				recipe_name: "Beer",
-				user_id: 1
-			},
-			{
-				event_id: 2,
-				recipe_name: "Amway",
-				user_id: 2
-			}
-		]);
-	});
+	// Inserts seed entries
+	return knex("recipes").insert([
+		{
+			event_id: 1,
+			recipe_name: "Pizza",
+			user_id: null
+		},
+		{
+			event_id: 1,
+			recipe_name: "Beer",
+			user_id: 1
+		},
+		{
+			event_id: 2,
+			recipe_name: "Amway",
+			user_id: 2
+		}
+	]);
 };
